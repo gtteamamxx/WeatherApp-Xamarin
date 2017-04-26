e77d72b9471e4e470c42a35f686c7daf33ee9cdb
Grzegorz S <grs4_98@live.com>
2017-04-26 20:49:51 +0200

    Base

---
+++ weatherxamarin/weatherxamarin/Behaviors/TimeSwipAnimationBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace weatherxamarin.Behaviors
+{
+    public class TimeSwipAnimationBehavior  : Behavior<Image>
+    {
+        protected override async void OnAttachedTo(Image bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TranslationY = 300;
+            await bindable.TranslateTo(0, 0, 1000);
+        }
+    }
+}
---
+++ weatherxamarin/weatherxamarin/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace weatherxamarin.ViewModels
+{
+    public class StartPageViewModel : BindableObject
+    {
+
+        public StartPageViewModel()
+        {
+
+
+        }
+    }
+}
